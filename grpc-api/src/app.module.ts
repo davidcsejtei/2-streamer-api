55e04259d6c38f3b045388c8ac5cebe8e904bf0a
csejtei1dav713 <csejteidavid90@gmail.com>
2025-09-26 15:05:56 +0200

    fixes

--- grpc-api/src/app.module.ts
+++ grpc-api/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { VideoController } from './video.controller';
-import { VideoService } from './video.service';
+import { VideoController } from './video/video.controller';
+import { VideoService } from './video/video.service';
 
 @Module({
   imports: [],
